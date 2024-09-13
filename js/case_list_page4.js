267b1e55f12767200eb34f43403a1ab3aef74f3b
javohir <javohir52664400456@gmail.com>
2024-09-13 14:39:38 +0500

    Javo: the case list page finished

--- js/case_list_page4.js
+++ js/case_list_page4.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var currentUrl = window.location.href;
-    var links = document.querySelectorAll('.case-filter-main a');
+    var links = document.querySelectorAll('.case-filter-main a, .case-pagination a');
     links.forEach(function(link) {
         if (link.href === currentUrl) {
             link.classList.add('active');
         }
     });
 });
+
